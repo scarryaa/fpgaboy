dd72f7ad6c72560e6ab12231224fe5424d418dba
scarlet <scarryaa@gmail.com>
2025-05-02 04:21:17 -0400

    reg file

--- src/verilog/cpu/cpu.sv
+++ src/verilog/cpu/cpu.sv
@@ -3,9 +3,25 @@ module cpu (
     input logic i_rst
 );
 
+  logic [2:0] reg_a_sel;
+  logic [2:0] reg_b_sel;
+  logic [2:0] reg_wr_sel;
+  logic reg_wr_en;
+  logic [7:0] reg_wr_data;
+  logic [7:0] reg_a;
+  logic [7:0] reg_b;
+
   reg_file u_reg_file (
       .i_clk(i_clk),
-      .i_rst(i_rst)
+      .i_rst(i_rst),
+
+      .i_reg_a_sel(reg_a_sel),
+      .i_reg_b_sel(reg_b_sel),
+      .i_reg_wr_sel(reg_wr_sel),
+      .i_reg_wr_en(reg_wr_en),
+      .i_reg_wr_data(reg_wr_data),
+      .o_reg_a(reg_a),
+      .o_reg_b(reg_b)
   );
 
 endmodule
--- src/verilog/cpu/reg_file.sv
+++ src/verilog/cpu/reg_file.sv
@@ -1,6 +1,33 @@
 module reg_file (
     input logic i_clk,
-    input logic i_rst
+    input logic i_rst,
+
+    input logic [2:0] i_reg_a_sel,
+    input logic [2:0] i_reg_b_sel,
+    input logic [2:0] i_reg_wr_sel,
+    input logic i_reg_wr_en,
+    input logic [7:0] i_reg_wr_data,
+    output logic [7:0] o_reg_a,
+    output logic [7:0] o_reg_b
 );
+
+  reg [7:0] registers[7:0];
+
+  always_ff @(posedge i_clk, posedge i_rst) begin
+    if (i_rst) begin
+      for (int i = 0; i < 8; i++) begin
+        registers[i] <= 8'd0;
+      end
+    end else begin
+      if (i_reg_wr_en) begin
+        registers[i_reg_wr_sel] <= i_reg_wr_data;
+      end
+    end
+  end
+
+  always_comb begin
+    o_reg_a = registers[i_reg_a_sel];
+    o_reg_b = registers[i_reg_b_sel];
+  end
 
 endmodule
