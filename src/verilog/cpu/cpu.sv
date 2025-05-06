97e0432a46e26105495f1f5d4becd2c843cb522d
scarlet <scarryaa@gmail.com>
2025-05-06 07:09:01 -0400

    or a,[hl], and a,[hl]

--- src/verilog/cpu/cpu.sv
+++ src/verilog/cpu/cpu.sv
@@ -110,6 +110,8 @@ module cpu (
   logic is_cp_a_r;
   logic is_add_a_hl_mem;
   logic is_sub_a_hl_mem;
+  logic is_and_a_hl_mem;
+  logic is_or_a_hl_mem;
 
   function [2:0] get_m_cycles(logic [7:0] ir);
     case (ir)
@@ -259,6 +261,8 @@ module cpu (
             is_and_a_r <= 1'b0;
             is_or_a_r <= 1'b0;
             is_add_a_hl_mem <= 1'b0;
+            is_and_a_hl_mem <= 1'b0;
+            is_or_a_hl_mem <= 1'b0;
 
             case (i_mem_rd_data)
               8'h41, 8'h42, 8'h43, 8'h44, 8'h45, 8'h47, 8'h48, 8'h4A, 8'h4B, 8'h4C, 8'h4D, 8'h4F, 
@@ -302,6 +306,8 @@ module cpu (
               8'hB8, 8'hB9, 8'hBA, 8'hBB, 8'hBC, 8'hBD, 8'hBF: is_cp_a_r <= 1'b1;
               8'h86: is_add_a_hl_mem <= 1'b1;
               8'h96: is_sub_a_hl_mem <= 1'b1;
+              8'hA6: is_and_a_hl_mem <= 1'b1;
+              8'hB6: is_or_a_hl_mem <= 1'b1;
 
               default: ;
             endcase
@@ -841,6 +847,20 @@ module cpu (
               reg_c_sel <= 3'd7;
               o_mem_rd_addr <= {reg_a, reg_b};
             end
+          end else if (is_and_a_hl_mem) begin
+            if (m_cycle == 1 && t_state == 0) begin
+              reg_a_sel <= 3'd4;
+              reg_b_sel <= 3'd5;
+              reg_c_sel <= 3'd7;
+              o_mem_rd_addr <= {reg_a, reg_b};
+            end
+          end else if (is_or_a_hl_mem) begin
+            if (m_cycle == 1 && t_state == 0) begin
+              reg_a_sel <= 3'd4;
+              reg_b_sel <= 3'd5;
+              reg_c_sel <= 3'd7;
+              o_mem_rd_addr <= {reg_a, reg_b};
+            end
           end
         end
 
@@ -1250,6 +1270,38 @@ module cpu (
 
               reg_wr_sel  <= 3'd7;
               reg_wr_data <= diff8;
+              reg_wr_en   <= 1'b1;
+            end
+          end else if (is_and_a_hl_mem) begin
+            if (m_cycle == 1 && t_state == 0) begin
+              logic [7:0] and8;
+
+              and8   = reg_c & i_mem_rd_data;
+
+              f[7]   = (and8 == 8'h00);
+              f[6]   = 1'b0;
+              f[5]   = 1'b1;
+              f[4]   = 1'b0;
+              f[3:0] = 4'b0000;
+
+              reg_wr_sel  <= 3'd7;
+              reg_wr_data <= and8;
+              reg_wr_en   <= 1'b1;
+            end
+          end else if (is_or_a_hl_mem) begin
+            if (m_cycle == 1 && t_state == 0) begin
+              logic [7:0] or8;
+
+              or8 = reg_c | i_mem_rd_data;
+
+              f[7] = (or8 == 8'h00);
+              f[6] = 1'b0;
+              f[5] = 1'b0;
+              f[4] = 1'b0;
+              f[3:0] = 4'b0000;
+
+              reg_wr_sel  <= 3'd7;
+              reg_wr_data <= or8;
               reg_wr_en   <= 1'b1;
             end
           end
