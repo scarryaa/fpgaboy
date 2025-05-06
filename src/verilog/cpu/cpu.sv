cfde8d33548a72397e75a5c846d8b580fe597724
scarlet <scarryaa@gmail.com>
2025-05-06 07:52:17 -0400

    xor a,n8, cp a,n8

--- src/verilog/cpu/cpu.sv
+++ src/verilog/cpu/cpu.sv
@@ -122,6 +122,8 @@ module cpu (
   logic is_or_a_n8;
   logic is_adc_a_n8;
   logic is_sbc_a_n8;
+  logic is_xor_a_n8;
+  logic is_cp_a_n8;
 
   function [2:0] get_m_cycles(logic [7:0] ir);
     case (ir)
@@ -176,7 +178,7 @@ module cpu (
           FETCH:          next_state = DECODE;
           DECODE: begin
             if (is_ld_a_imm16 || is_ld_imm16_a || is_ld_rr_imm) next_state = FETCH_IMM16_LO;
-            else if (is_ld_r_imm || is_ld_hl_sp_e8 || is_add_sp_e8 || is_add_a_n8 || is_sub_a_n8 || is_and_a_n8 || is_or_a_n8 || is_adc_a_n8 || is_sbc_a_n8)
+            else if (is_ld_r_imm || is_ld_hl_sp_e8 || is_add_sp_e8 || is_add_a_n8 || is_sub_a_n8 || is_and_a_n8 || is_or_a_n8 || is_adc_a_n8 || is_sbc_a_n8 || is_xor_a_n8 || is_cp_a_n8)
               next_state = FETCH_IMM;
             else if (is_ld_hl_imm) next_state = FETCH_IMM;
             else if (is_ldh_imm_a) next_state = FETCH_IMM;
@@ -285,6 +287,8 @@ module cpu (
             is_or_a_n8 <= 1'b0;
             is_adc_a_n8 <= 1'b0;
             is_sbc_a_n8 <= 1'b0;
+            is_xor_a_n8 <= 1'b0;
+            is_cp_a_n8 <= 1'b0;
 
             case (i_mem_rd_data)
               8'h41, 8'h42, 8'h43, 8'h44, 8'h45, 8'h47, 8'h48, 8'h4A, 8'h4B, 8'h4C, 8'h4D, 8'h4F, 
@@ -340,6 +344,8 @@ module cpu (
               8'hF6: is_or_a_n8 <= 1'b1;
               8'hCE: is_adc_a_n8 <= 1'b1;
               8'hDE: is_sbc_a_n8 <= 1'b1;
+              8'hEE: is_xor_a_n8 <= 1'b1;
+              8'hFE: is_cp_a_n8 <= 1'b1;
 
               default: ;
             endcase
@@ -942,6 +948,14 @@ module cpu (
               reg_a_sel <= 3'd7;
             end
           end else if (is_sbc_a_n8) begin
+            if (m_cycle == 1 && t_state == 0) begin
+              reg_a_sel <= 3'd7;
+            end
+          end else if (is_xor_a_n8) begin
+            if (m_cycle == 1 && t_state == 0) begin
+              reg_a_sel <= 3'd7;
+            end
+          end else if (is_cp_a_n8) begin
             if (m_cycle == 1 && t_state == 0) begin
               reg_a_sel <= 3'd7;
             end
@@ -1567,6 +1581,35 @@ module cpu (
               reg_wr_sel  <= 3'd7;
               reg_wr_data <= sbc8;
               reg_wr_en   <= 1'b1;
+            end
+          end else if (is_xor_a_n8) begin
+            if (m_cycle == 1 && t_state == 0) begin
+              logic [7:0] xor8;
+              xor8   = reg_a ^ imm;
+
+              f[7]   = (xor8 == 8'h00);
+              f[6]   = 1'b0;
+              f[5]   = 1'b0;
+              f[4]   = 1'b0;
+              f[3:0] = 4'b0000;
+
+              reg_wr_sel  <= 3'd7;
+              reg_wr_data <= xor8;
+              reg_wr_en   <= 1'b1;
+            end
+          end else if (is_cp_a_n8) begin
+            if (m_cycle == 1 && t_state == 0) begin
+              logic [7:0] diff8;
+              logic [8:0] diff9;
+
+              diff8  = reg_a - imm;
+              diff9  = {1'b0, reg_a} - {1'b0, imm};
+
+              f[7]   = (diff8 == 8'h00);
+              f[6]   = 1'b1;
+              f[5]   = ((reg_a & 8'h0F) < (imm & 8'h0F)) ? 1'b1 : 1'b0;
+              f[4]   = diff9[8];
+              f[3:0] = 4'b0000;
             end
           end
         end
