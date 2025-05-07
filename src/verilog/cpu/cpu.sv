fa47fd3ba7eff79da5928e68ec8409a530a8ba2e
scarlet <scarryaa@gmail.com>
2025-05-07 01:20:09 -0400

    ret nz, ret nc

--- src/verilog/cpu/cpu.sv
+++ src/verilog/cpu/cpu.sv
@@ -163,6 +163,8 @@ module cpu (
   logic is_push_de;
   logic is_push_hl;
   logic is_push_af;
+  logic is_ret_nz;
+  logic is_ret_nc;
 
   function [2:0] get_m_cycles(logic [7:0] ir);
     case (ir)
@@ -197,6 +199,7 @@ module cpu (
       8'hC3: get_m_cycles = 4;
       8'hC1, 8'hD1, 8'hE1, 8'hF1: get_m_cycles = 3;
       8'hC5, 8'hD5, 8'hE5, 8'hF5: get_m_cycles = 4;
+      8'hC0, 8'hD0: get_m_cycles = 5;
 
       default: get_m_cycles = 1;
     endcase
@@ -404,6 +407,8 @@ module cpu (
             is_push_de <= 1'b0;
             is_push_hl <= 1'b0;
             is_push_af <= 1'b0;
+            is_ret_nz <= 1'b0;
+            is_ret_nc <= 1'b0;
 
             case (i_mem_rd_data)
               8'h41, 8'h42, 8'h43, 8'h44, 8'h45, 8'h47, 8'h48, 8'h4A, 8'h4B, 8'h4C, 8'h4D, 8'h4F, 
@@ -493,6 +498,8 @@ module cpu (
               8'hD5: is_push_de <= 1'b1;
               8'hE5: is_push_hl <= 1'b1;
               8'hF5: is_push_af <= 1'b1;
+              8'hC0: is_ret_nz <= 1'b1;
+              8'hD0: is_ret_nc <= 1'b1;
 
               default: ;
             endcase
@@ -1324,6 +1331,38 @@ module cpu (
               o_mem_wr_addr <= sp - 16'd1; 
               o_mem_wr_en   <= 1'b1;
             end
+          end if (is_ret_nz) begin
+            if (m_cycle == 0 && t_state == 0) begin
+              if (!f[7]) begin 
+                M_CYCLE_MAX <= 4; 
+                o_mem_rd_addr <= sp; 
+              end else begin
+                M_CYCLE_MAX <= 1; 
+              end
+            end
+            if (!f[7]) begin
+              if (m_cycle == 0 && t_state == 3) begin
+                prev_mem_rd_data <= i_mem_rd_data; 
+              end else if (m_cycle == 1 && t_state == 0) begin
+                o_mem_rd_addr <= sp + 1; 
+              end
+            end
+          end else if (is_ret_nc) begin
+            if (m_cycle == 0 && t_state == 0) begin
+              if (!f[4]) begin 
+                M_CYCLE_MAX <= 4; 
+                o_mem_rd_addr <= sp;
+              end else begin
+                M_CYCLE_MAX <= 1; 
+              end
+            end
+            if (!f[4]) begin
+              if (m_cycle == 0 && t_state == 3) begin
+                prev_mem_rd_data <= i_mem_rd_data; 
+              end else if (m_cycle == 1 && t_state == 0) begin
+                o_mem_rd_addr <= sp + 1;
+              end
+            end
           end
         end
 
@@ -2000,6 +2039,16 @@ module cpu (
               reg_wr_data <= i_mem_rd_data;
               reg_wr_en   <= 1'b1;
               f = {prev_mem_rd_data & 8'hF0};
+              sp <= sp + 16'd2;
+            end
+          end else if (is_ret_nz && !f[7]) begin
+            if (m_cycle == 1 && t_state == 0) begin
+              pc <= {i_mem_rd_data, prev_mem_rd_data}; 
+              sp <= sp + 16'd2;
+            end
+          end else if (is_ret_nc && !f[4]) begin
+            if (m_cycle == 1 && t_state == 0) begin
+              pc <= {i_mem_rd_data, prev_mem_rd_data};
               sp <= sp + 16'd2;
             end
           end
